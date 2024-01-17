e796c157796e71aabca219a7701eb13753c65253
Santosh <santoanand2@gmail.com>
2024-01-17 15:05:31 -0700

    Added IP address of server and print statements for debugging

--- src/client.py
+++ src/client.py
@@ -18,7 +18,6 @@ def make_route_note(message):
 def generate_messages():
     #while(True):
     for _ in range(100):
-            #leep(100)
             msg = make_route_note("Msg# "+str(_))
             print("Sending %d", _)
             yield msg
@@ -28,6 +27,7 @@ def guide_route_chat(stub):
     msg = ""
     for _ in np.arange(70*100*100).tolist():
         msg+=str(_)
+    total_lag = 0
     for _ in range(100):
         start_time = int(time.time_ns())
         #time.sleep(1)
@@ -36,15 +36,17 @@ def guide_route_chat(stub):
                 "Received message %d" % (_)
             )
         end_time = int(time.time_ns())
-        print("Lag is: %d", (end_time - start_time)/1000000)
+        #print("Lag is: %d", (end_time - start_time)/1000000)
+        total_lag+=(end_time - start_time)/1000000
 
+    print("Total lag across 100 request to the remote server is: %dms" %(total_lag/100))
 
 
 def run():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
-    with grpc.insecure_channel("localhost:50051") as channel:
+    with grpc.insecure_channel("192.168.122.1:50051") as channel:
         stub = memory_check_pb2_grpc.MemoryCheckStub(channel)
         guide_route_chat(stub)
 
