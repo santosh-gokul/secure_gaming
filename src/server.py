ee0c04795ea859eaa2f4d174ddc3ffcf1545a4a8
Santosh <santoanand2@gmail.com>
2024-01-17 15:14:55 -0700

    Changed listening IP of the server to 0.0.0.0

--- src/server.py
+++ src/server.py
@@ -22,7 +22,7 @@ def serve():
     memory_check_pb2_grpc.add_MemoryCheckServicer_to_server(
         MemoryCheckServicer(), server
     )
-    server.add_insecure_port("[::]:50051")
+    server.add_insecure_port("0.0.0.0:50051")
     server.start()
     server.wait_for_termination()
 
